2805d48e90d4b2361bf1207b5e2bf8725b541c79
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-07 23:17:14 +0200

    Amend.

--- Gu.Inject.Benchmarks/Benchmarks/Get.cs
+++ Gu.Inject.Benchmarks/Benchmarks/Get.cs
@@ -3,19 +3,27 @@
     using BenchmarkDotNet.Attributes;
     using Gu.Inject.Benchmarks.Types;
     using Ninject;
+    using SimpleInjector;
 
     public class Get
     {
         private static readonly Ninject.StandardKernel StandardKernel = new Ninject.StandardKernel();
+        private static readonly Container Container = new Container();
         private static readonly Kernel Kernel = new Kernel();
 
-        [Benchmark(Baseline = true)]
-        public object NinjectGetFoo()
+        [Benchmark]
+        public object Ninject()
         {
             return StandardKernel.Get<Foo>();
         }
 
         [Benchmark]
+        public object SimpleInjector()
+        {
+            return Container.GetInstance<Foo>();
+        }
+
+        [Benchmark(Baseline = true)]
         public object GuInject()
         {
             return Kernel.Get<Foo>();
--- Gu.Inject.Benchmarks/Benchmarks/NewAndGet.cs
+++ Gu.Inject.Benchmarks/Benchmarks/NewAndGet.cs
@@ -3,10 +3,11 @@
     using BenchmarkDotNet.Attributes;
     using Gu.Inject.Benchmarks.Types;
     using Ninject;
+    using SimpleInjector;
 
     public class NewAndGet
     {
-        [Benchmark(Baseline = true)]
+        [Benchmark]
         public object Ninject()
         {
             using (var kernel = new Ninject.StandardKernel())
@@ -16,6 +17,15 @@
         }
 
         [Benchmark]
+        public object SimpleInjector()
+        {
+            using (var container = new Container())
+            {
+                return container.GetInstance<Foo>();
+            }
+        }
+
+        [Benchmark(Baseline = true)]
         public object GuInject()
         {
             using (var kernel = new Kernel())
