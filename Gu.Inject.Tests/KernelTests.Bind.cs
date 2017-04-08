57fb7c872dd33d2d5ca4248564efc8520f74aeb3
Johan Larsson <johan_gavle1@hotmail.com>
2017-04-08 08:47:16 +0200

    Tests for Bind.

--- Gu.Inject.Tests/KernelTests.Bind.cs
+++ Gu.Inject.Tests/KernelTests.Bind.cs
@@ -10,14 +10,23 @@
     {
         public class Bind
         {
-            [Test]
-            public void BindThenGet()
+            [TestCase(typeof(IWith), typeof(With<DefaultCtor>))]
+            [TestCase(typeof(OneToMany.Abstract), typeof(OneToMany.Concrete1))]
+            [TestCase(typeof(OneToMany.Abstract), typeof(OneToMany.Concrete2))]
+            [TestCase(typeof(OneToMany.IAbstract), typeof(OneToMany.Concrete1))]
+            [TestCase(typeof(OneToMany.IAbstract), typeof(OneToMany.Concrete2))]
+            [TestCase(typeof(OneToMany.IConcrete), typeof(OneToMany.Concrete1))]
+            [TestCase(typeof(OneToMany.IConcrete), typeof(OneToMany.Concrete2))]
+            [TestCase(typeof(OneToMany.IConcrete), typeof(OneToMany.Concrete2))]
+            [TestCase(typeof(With<OneToMany.IConcrete>), typeof(With<OneToMany.Concrete2>))]
+            [TestCase(typeof(IWith<OneToMany.IConcrete>), typeof(With<OneToMany.Concrete2>))]
+            public void BindThenGet(Type from, Type to)
             {
                 using (var kernel = new Kernel())
                 {
-                    kernel.Bind<IWith, With<DefaultCtor>>();
-                    var actual = kernel.Get<IWith>();
-                    Assert.AreSame(actual, kernel.Get<With<DefaultCtor>>());
+                    kernel.Bind(from, to);
+                    var actual = kernel.Get(from);
+                    Assert.AreSame(actual, kernel.Get(to));
                 }
             }
 
---
+++ Gu.Inject.Tests/Types/IWithTwo{T1, T2}.cs
@@ -0,0 +1,9 @@
+﻿namespace Gu.Inject.Tests.Types
+{
+    public interface IWithTwo<T1, T2>
+    {
+        T1 Value1 { get; }
+
+        T2 Value2 { get; }
+    }
+}
