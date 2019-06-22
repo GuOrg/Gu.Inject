89740faf769dbfcf0b7439c3e42f7c34ad026486
Johan Larsson <johan_gavle1@hotmail.com>
2019-06-22 07:19:15 +0200

    IncorrectAutoBind WIP

---
+++ Gu.Inject.Analyzers/GuInj001IncorrectAutoBind.cs
@@ -0,0 +1,19 @@
+﻿namespace Gu.Inject.Analyzers
+{
+    using Microsoft.CodeAnalysis;
+
+    public static class GuInj001IncorrectAutoBind
+    {
+        public const string DiagnosticId = "GUINJ001";
+
+        public static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
+            id: DiagnosticId,
+            title: "Regenerate bindings.",
+            messageFormat: "Regenerate bindings.",
+            category: AnalyzerCategory.Correctness,
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: "The bindings are incorrect and must be regenerated.",
+            helpLinkUri: HelpLink.ForId(DiagnosticId));
+    }
+}
---
+++ Gu.Inject.Analyzers/NodeAnalyzers/MethodDeclarationAnalyzer.cs
@@ -0,0 +1,37 @@
+﻿namespace Gu.Inject.Analyzers.NodeAnalyzers
+{
+    using System.Collections.Immutable;
+    using Gu.Roslyn.AnalyzerExtensions;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MethodDeclarationAnalyzer : DiagnosticAnalyzer
+    {
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+            GuInj001IncorrectAutoBind.Descriptor);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(Handle, SyntaxKind.MethodDeclaration);
+        }
+
+        private static void Handle(SyntaxNodeAnalysisContext context)
+        {
+            if (context.ContainingSymbol is IMethodSymbol method &&
+                method.HasCompilerGeneratedAttribute() &&
+                method.Parameters.TrySingle(out var parameter) &&
+                parameter.Type == KnownSymbol.KernelOfT &&
+                parameter.Type is INamedTypeSymbol kernelType &&
+                kernelType.TypeArguments.TrySingle(out var rootType))
+            {
+
+            }
+        }
+    }
+}
