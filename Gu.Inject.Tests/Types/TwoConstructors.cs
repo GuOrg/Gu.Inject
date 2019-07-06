85adc88cda4ac8c4115cc7a08f6ad96cdf884db0
JohanLarsson <johan_gavle1@hotmail.com>
2019-07-06 17:07:24 +0200

    Don't generate AutoBinding when more than one ctor.

---
+++ Gu.Inject.Tests/Types/TwoConstructors.cs
@@ -0,0 +1,16 @@
+﻿namespace Gu.Inject.Tests.Types
+{
+    public class TwoConstructors
+    {
+        public TwoConstructors()
+        {
+        }
+
+        public TwoConstructors(Bar bar)
+        {
+            this.Bar = bar;
+        }
+
+        public Bar Bar { get; }
+    }
+}
