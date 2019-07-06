7e1efc8ab0e28e8bdb4954d94b35e51487a0149a
JohanLarsson <johan_gavle1@hotmail.com>
2019-07-06 10:47:14 +0200

    Fix tests.

---
+++ Gu.Inject.Analyzers/Internals/SyntaxGeneratorExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace Gu.Inject.Analyzers
+{
+    using System;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Editing;
+
+    public static class SyntaxGeneratorExtensions
+    {
+        public static TypeSyntax CrefType(this SyntaxGenerator g, ITypeSymbol type)
+        {
+            return GenericCrefRewriter.Update((TypeSyntax)g.TypeExpression(type));
+        }
+
+        private class GenericCrefRewriter : CSharpSyntaxRewriter
+        {
+            private static readonly GenericCrefRewriter Default = new GenericCrefRewriter();
+
+            public static TypeSyntax Update(TypeSyntax node) => (TypeSyntax)Default.Visit(node);
+
+            public override SyntaxNode VisitTypeArgumentList(TypeArgumentListSyntax node)
+            {
+                return base.VisitTypeArgumentList(node)
+                           .ReplaceTokens(
+                               new[] { node.LessThanToken, node.GreaterThanToken },
+                               (x, _) => Replace(x));
+
+                SyntaxToken Replace(SyntaxToken old)
+                {
+                    switch (old.Kind())
+                    {
+                        case SyntaxKind.GreaterThanToken:
+                            return SyntaxFactory.Token(
+                                leading: old.LeadingTrivia,
+                                kind: SyntaxKind.GreaterThanToken,
+                                text: "}",
+                                valueText: ">",
+                                trailing: old.TrailingTrivia);
+
+                        case SyntaxKind.LessThanToken:
+                            return SyntaxFactory.Token(
+                                leading: old.LeadingTrivia,
+                                kind: SyntaxKind.LessThanToken,
+                                text: "{",
+                                valueText: "<",
+                                trailing: old.TrailingTrivia);
+                        default:
+                            throw new InvalidOperationException("Not getting here.");
+                    }
+                }
+            }
+        }
+    }
+}
