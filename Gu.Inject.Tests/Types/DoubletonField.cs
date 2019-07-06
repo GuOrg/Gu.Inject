fd8a7960ca15a880208c6918cd702d6993b27814
JohanLarsson <johan_gavle1@hotmail.com>
2019-07-06 18:35:02 +0200

    Doubletons

---
+++ Gu.Inject.Tests/Types/DoubletonField.cs
@@ -0,0 +1,13 @@
+﻿namespace Gu.Inject.Tests.Types
+{
+    public sealed class DoubletonField
+    {
+        public static readonly DoubletonField Instance1 = new DoubletonField();
+
+        public static readonly DoubletonField Instance2 = new DoubletonField();
+
+        private DoubletonField()
+        {
+        }
+    }
+}
---
+++ Gu.Inject.Tests/Types/DoubletonProperty.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Inject.Tests.Types
+{
+    public sealed class DoubletonProperty
+    {
+        public static DoubletonProperty Instance1 { get; } = new DoubletonProperty();
+        public static DoubletonProperty Instance2 { get; } = new DoubletonProperty();
+
+        private DoubletonProperty()
+        {
+        }
+    }
+}
