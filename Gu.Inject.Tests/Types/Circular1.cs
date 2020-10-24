abddbb9ec5eaceb0ea28f3d26d3947509fd45a75
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-24 12:56:48 +0200

    Add properties to Circular.

--- Gu.Inject.Tests/Types/Circular1.cs
+++ Gu.Inject.Tests/Types/Circular1.cs
@@ -9,14 +9,20 @@ namespace Gu.Inject.Tests.Types
         {
             public A(B b)
             {
+                this.B = b;
             }
+
+            public B B { get; }
         }
 
         public class B
         {
             public B(A a)
             {
+                this.A = a;
             }
+
+            public A A { get; }
         }
     }
 }
